be822e0a52a514ce0a348c2b8846bc8c972a3038
wangshiyuan <wangshiyuan@navinfo.com>
2020-07-10 10:02:23 +0800

    inject browser-sync

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -2,14 +2,18 @@
 // 1.require()导入模块
 // 2.使用模块上的命令
 
-
-// 控制台，gulp task名 运行任务
-
 const gulp = require('gulp');
 
+// 控制台，gulp task名 运行任务
 // 编写任务，参数：名称，回调函数
 gulp.task('hello', function () {
   console.info('Hello WOrld!')
+})
+
+// 0.任务前先清除目录
+const del = require('del');
+gulp.task('clean', function(){
+  del (['./dist'])
 })
 
 // gulp.src() // 找到资源文件
@@ -36,10 +40,6 @@ gulp.task('data', function () {
   .pipe(gulp.dest('dist/data'))
 })
 
-// 4.一次执行多个任务
-gulp.task('build', ['copy-html', 'images', 'data'], function () {
-  console.info('任务执行完毕！')
-})
 
 // 5.任务监听
 // 文件改变，会自动执行任务
@@ -87,10 +87,28 @@ gulp.task('JS', function () {
     .pipe(gulp.dest('dist/js'))
 })
 
+
+var inject = require('gulp-inject');
+// inject 注入
+gulp.task('inject', ['build'], function() {
+  return gulp.src('./dist/index.html') // 获取该文件的数据 
+    .pipe(inject(gulp.src(['./dist/js/index.min.js','./dist/css/index.min.css'], { // 让获取的流被 inject 操作一次：把指定文件注入到流文件中（也就是把 app.js 那段外部链接的代码写到 index.html中）
+      read: false, // 暂时不用管什么意
+      relative: true // 暂时不用管什么意
+    })))
+    .pipe(gulp.dest('./dist')) // 把被操作过的这个文件流放到 dist 文件夹下 
+})
+
+// 4.一次执行多个任务
+gulp.task('build', ['clean', 'copy-html', 'images', 'data', 'sass-min', 'JS'], function () {
+  
+})
+
 // 5.任务监听
 // 文件改变，会自动执行任务
 gulp.task('watch', function () {
   gulp.watch('index.html', ['copy-html'])
+  gulp.watch('style/**/*.scss', ['sass-min'])
   gulp.watch('img/**/*', ['images']);
   gulp.watch(['json/*.json', 'xml/*.xml', '!xml/old.xml'], ['data']);
   gulp.watch('js/*.js', ['JS'])
@@ -102,12 +120,21 @@ gulp.task('server', function () {
   connect.server({
     root: 'dist',
     port: 9001,
-    livereload: true // 实时刷新
+    livereload: true, // 实时刷新
+    open: true // 自动打开浏览器
+  })
+})
+
+const browserSync = require('browser-sync');
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: './',
+    port: 9002,
+    files: [
+      './dist/**/*.*'
+    ]
   })
 })
 
 // 10.默认任务，cmd中不需写任务名
-gulp.task('default', ['watch', 'server'])
-
-
-
+gulp.task('default', ['build', 'watch', 'inject', 'browserSync'])
